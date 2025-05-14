449bf2d5549be1d98265b264d7dbdb37673a260a
Melissa Avery-Weir <averymd@irrsinn.net>
2025-05-14 13:47:25 -0400

    Okay, let's try with declaring a bit of it

--- boards/shields/nice_oled/widgets/modifiers.c
+++ boards/shields/nice_oled/widgets/modifiers.c
@@ -37,6 +37,7 @@ const lv_img_dsc_t *luna_imgs_sit_90[] = {&dog_sit1_90, &dog_sit2_90};
 const lv_img_dsc_t *luna_imgs_walk_90[] = {&dog_walk1_90, &dog_walk2_90};
 const lv_img_dsc_t *luna_imgs_run_90[] = {&dog_run1_90, &dog_run2_90};
 const lv_img_dsc_t *luna_imgs_sneak_90[] = {&dog_sneak1_90, &dog_sneak2_90};
+const lv_img_dsc_t *luna_imgs_bark_90[] = {&dog_bark1_90, &dog_bark2_90};
 
 static lv_obj_t *luna_imgs = NULL;
 
